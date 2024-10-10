c674e24adcdbb26be0cd17d2d6a590b9e958fdc5
partu25 <143474168+partu25@users.noreply.github.com>
2024-10-10 20:44:53 +0530

    Add files via upload

---
+++ booklist/backend/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import {Book} from './models/bookModel.js';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
+const app = express();
+
+//Middleware for parsing request body
+app.use(express.json());
+
+
+//middleware for handling CORS policy
+// option 1: allow all origins with default of cors(*)
+app.use(cors());
+// option 2: allow custom origins
+// app.use(
+//     cors({
+//         origin:'http://localhost:3000',
+//         methods:['GET','POST','PUT','DELETE'],
+//         allowedHeaders:['Content-Type']
+//     })
+// );
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Welcome to my project");
+});
+
+app.use('/books',booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
